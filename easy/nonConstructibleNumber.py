fc7838496a360fc2382ddaefd869114aebfb03b0
Kuttyhub <nishanth1216@gmail.com>
2022-02-27 23:31:51 +0530

    implimented optimal way to solve nonConstructiable number

--- easy/nonConstructibleNumber.py
+++ easy/nonConstructibleNumber.py
@@ -1,4 +1,8 @@
-def nonConstructibleChange(coins):
+from array import array
+
+
+def nonConstructibleChangeBruteForce(coins):
+    #brute force method
     coins.sort()
     print(coins)
     res = set()
@@ -11,11 +15,31 @@ def nonConstructibleChange(coins):
         res=set.union(res,temp)
     res = sorted(list(res))
     print(res)
+    # array = [1, 1, 2, 3, 5, 7, 22]
+    # {1}
+    # {1, 2}
+    # {1, 2, 3, 4}
+    # {1, 2, 3, 4, 5, 6, 7}
+    # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+    # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
+    # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41}
     coinValue = 1
     while len(res)>0 and coinValue == res[0]:
         res.pop(0)
         coinValue+=1
     return coinValue
+
+def nonConstructibleChange(coins):
+    #optimal way
+    coins.sort()
+    currenMaxChange =0
+    for i in coins:
+        if currenMaxChange+1 >=i:
+            currenMaxChange+=i
+        else:
+            return currenMaxChange+1
+    return currenMaxChange+1
+ 
 
 coins=[109, 2000, 8765, 19, 18, 17, 16, 8, 1, 1, 2, 4]
 print(nonConstructibleChange(coins))

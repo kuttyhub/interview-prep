8c8f413d8a1a835087435845ba6ab7a257cf9168
Kuttyhub <nishanth1216@gmail.com>
2022-04-26 14:24:09 +0530

    added rotate matrix problem

--- CrackingCodingInterview/arrays/rotateMatrix.py
+++ CrackingCodingInterview/arrays/rotateMatrix.py
@@ -1,2 +1,21 @@
 def rotate_matrix(matrix):
-    pass
+    res =[]
+    n = len(matrix)
+    for i in range(n):
+        temp = []
+        for j in range(n):
+            temp.insert(0,matrix[j][i])
+        res.append(temp)
+    print(res)
+
+n = 5
+array =[]
+
+k =1
+for i in range(n):
+    temp = []
+    for j in range(n):
+        temp.append(k)
+        k+=1
+    array.append(temp)
+rotate_matrix(array)
